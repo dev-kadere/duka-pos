29c231c38273291c4e61423d764fc5871273277a
dev-kadere <ictnicholasndereba@gmail.com>
2026-02-15 22:54:54 +0300

    dashbord component

--- app/(dashbord)/dashboard/page.tsx
+++ app/(dashbord)/dashboard/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import DashboardPage from "../layout";
+import DashboardLayout from "../layout";
 
 const page = () => {
-  return <div>page</div>;
+  return <h1 className="text-3xl font-bold mb-4">Dashboard Home</h1>;
 };
 
 export default page;
